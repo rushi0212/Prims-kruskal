2c8014b7991b5765149599cb1f6a4b57fab30a7b
rushi0212 <152675340+rushi0212@users.noreply.github.com>
2023-12-02 11:34:45 +0530

    Create primsusindlist.c

---
+++ primsusindlist.c
@@ -0,0 +1,94 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Structure for a node in the adjacency list
+struct Node {
+    int vertex;
+    struct Node* next;
+};
+
+// Structure for the adjacency list
+struct AdjList {
+    struct Node* head;
+};
+
+// Structure for the graph
+struct Graph {
+    int vertices;
+    struct AdjList* array;
+};
+
+// Function to create a new node
+struct Node* createNode(int vertex) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->vertex = vertex;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to create a graph with a given number of vertices
+struct Graph* createGraph(int vertices) {
+    struct Graph* graph = (struct Graph*)malloc(sizeof(struct Graph));
+    graph->vertices = vertices;
+
+    // Create an array of adjacency lists
+    graph->array = (struct AdjList*)malloc(vertices * sizeof(struct AdjList));
+
+    // Initialize each adjacency list as empty by setting head to NULL
+    for (int i = 0; i < vertices; ++i) {
+        graph->array[i].head = NULL;
+    }
+
+    return graph;
+}
+
+// Function to add an edge to the graph
+void addEdge(struct Graph* graph, int src, int dest) {
+    // Add an edge from src to dest
+    struct Node* newNode = createNode(dest);
+    newNode->next = graph->array[src].head;
+    graph->array[src].head = newNode;
+
+    // Since the graph is undirected, add an edge from dest to src as well
+    newNode = createNode(src);
+    newNode->next = graph->array[dest].head;
+    graph->array[dest].head = newNode;
+}
+
+// Function to print the adjacency list representation of the graph
+void printGraph(struct Graph* graph) {
+    for (int i = 0; i < graph->vertices; ++i) {
+        struct Node* temp = graph->array[i].head;
+        printf("Adjacency list of vertex %d:\n", i);
+        while (temp) {
+            printf("%d -> ", temp->vertex);
+            temp = temp->next;
+        }
+        printf("NULL\n");
+    }
+}
+
+int main() {
+    int vertices = 6; // 6 vertices for a prism
+    struct Graph* graph = createGraph(vertices);
+
+    // Add edges to represent the prism
+    addEdge(graph, 0, 1);
+    addEdge(graph, 1, 2);
+    addEdge(graph, 2, 3);
+    addEdge(graph, 3, 4);
+    addEdge(graph, 4, 5);
+    addEdge(graph, 5, 0);
+
+    addEdge(graph, 0, 2);
+    addEdge(graph, 1, 3);
+    addEdge(graph, 2, 4);
+    addEdge(graph, 3, 5);
+    addEdge(graph, 4, 0);
+    addEdge(graph, 5, 1);
+
+    // Print the adjacency list representation of the graph
+    printGraph(graph);
+
+    return 0;
+}
